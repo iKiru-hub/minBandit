e50af7016b014909479f43d1ffd431a5956a915c
Krubeal Danieli <daniekru@srl-login1.cm.cluster>
2024-06-06 11:34:30 +0200

    +ex3

--- slurm_run_main_evo.sh
+++ slurm_run_main_evo.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 ##SBATCH --mem-per-cpu=1GB
-#SBATCH --time=0-10:00
+#SBATCH --time=3-10:00
 #SBATCH -o /home/daniekru/slurm.column.%j.%N.out # STDOUT
 #SBATCH -e /home/daniekru/slurm.column.%j.%N.err # STDERR
 
