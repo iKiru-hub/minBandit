7f5d1c917fce9c31cc20a1d517196cd1b0806ac5
Krubeal Danieli <daniekru@srl-login1.cm.cluster>
2024-07-10 02:02:45 +0200

    fast update

--- slurm_run_main_evo.sh
+++ slurm_run_main_evo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name="mkb"
-#SBATCH -p milanq #armq #milanq #fpgaq #milanq # partition (queue)
+#SBATCH -p ipuq #milanq #armq #milanq #fpgaq #milanq # partition (queue)
 #SBATCH -N 1 # number of nodes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=170
+#SBATCH --cpus-per-task=1
 ##SBATCH --mem-per-cpu=1GB
 #SBATCH --time=1-10:00
 #SBATCH -o /home/daniekru/slurm.column.%j.%N.out # STDOUT
@@ -18,7 +18,7 @@ echo "$(pwd)"
 #srun python3 evo_main.py --verbose
 # srun python3 src/make_plot.py --run "smooth" --save
 #srun python3 main.py --verbose --trials 600 --rounds 1 --K 10 --reps 10 --plot --env "smooth" --load --save --idx 4 --multiple 2
-srun python3 main.py --verbose --trials 3 --rounds 3000 --K 10 --reps 20 --plot --env "simple" --load --save --idx 4 --multiple 2
+srun python3 main.py --verbose --trials 1000 --rounds 3 --K 200 --reps 20 --plot --env "smooth" --load --save --idx 4 --multiple 2
 
 echo "finished"
 
