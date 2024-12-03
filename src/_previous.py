1f47aff9e9e604b512d633d772bc7cc0868def2e
Krubeal <dkirubel@gmail.com>
2024-12-03 11:56:32 +0100

    fast update

--- src/_previous.py
+++ src/_previous.py
@@ -329,3 +329,182 @@ class KArmedBanditSmoothII:
 
 
 
+class Env2:
+
+    """
+    The game class for a k-arm bandit task.
+    """
+
+    def __init__(self, protocol: dict,
+                 verbose: bool,
+                 env_type: str="simple"):
+
+        """
+        The game class.
+
+        Parameters
+        ----------
+        game : envs.Environment
+            The environment.
+        protocol : dict
+            The protocol for a trial.
+        probabilities_set : list
+            The set of probabilities for each round.
+        verbose : bool
+            Whether to print information.
+        env_type: str
+            type of kbandit environment, options are
+            `simple` or `smooth`.
+            Default `simple`.
+        """
+
+        #
+        self.fitness_size = 1
+        self.protocol = protocol
+        self.verbose = verbose
+        self.env_type = env_type
+
+        self.K = protocol["K"]
+        self.tau = protocol["tau"]
+        self.nb_trials = protocol["nb_trials"]
+        self.nb_rounds = protocol["nb_rounds"]
+        self.nb_reps = protocol["nb_reps"]
+
+    def __repr__(self):
+
+        return f"Env({self.nb_reps}:{self.nb_trials}:{self.nb_rounds}, K={self.K}, env_type={self.env_type})"
+
+    def _make_env(self, env_type: str, nb_trials: int, K: int=None) -> object:
+
+        """
+        Make the environment.
+
+        Parameters
+        ----------
+        env_type : str
+            The type of environment.
+        nb_trials : int
+            The number of trials.
+        K : int
+            The number of bandits.
+
+        Returns
+        -------
+        env : object
+            The environment object.
+        """
+
+        if K is None:
+            K = self.K
+
+        normalize = False
+        fixed_p = 0.7
+
+        probabilities_set = make_probability_set(K=K,
+                                                 nb_trials=nb_trials,
+                                                 fixed_p=fixed_p,
+                                                 normalize=normalize)
+
+        if env_type == "simple":
+            return envs.KArmedBandit(K=K,
+                                     probabilities_set=probabilities_set)
+        elif env_type == "smooth2":
+            return envs.KArmedBanditSmoothII(
+                             K=K,
+                             verbose=False,
+                             tau=self.tau,
+                             fixed_p=fixed_p,
+                             normalize=normalize)
+        else:
+            return envs.KArmedBanditSmooth(
+                             K=K,
+                             probabilities_set=probabilities_set,
+                             tau=self.tau)
+
+    def run(self, agent: object) -> float:
+
+        """
+        Evaluate the agent on the k-arm bandit task.
+
+        Parameters
+        ----------
+        agent : object
+            The agent object.
+
+        Returns
+        -------
+        fitness : float
+            The fitness value.
+        """
+
+        ### environment ###
+        # environment & game
+        # env = self._make_env()
+
+        ### model ###
+        # params = agent.get_genome()
+        # params['K'] = self.K
+        # model = mm.Model(**params)
+
+        ### fit ###
+        # stats = envs.trial_multi_model(models=[model],
+        #                                environment=env,
+        #                                nb_trials=self.nb_trials,
+        #                                nb_rounds=self.nb_rounds,
+        #                                nb_reps=self.nb_reps)
+
+        # return calc_fitness(stats=stats)
+
+        # variant: multiple K
+        # Ks = [5, 15, 40]
+        Ks = [5, 35]
+
+        fitness = 0.
+        for env_type in ("simple", "smooth", "smooth2"):
+
+            # config-overwriting settings <<<< ! >>>>
+            nb_trials = 3 if env_type == "simple" else self.nb_trials
+            nb_rounds = 200 if env_type == "simple" else self.nb_rounds
+
+            for K in Ks:
+                env = self._make_env(env_type=env_type,
+                                     nb_trials=nb_trials,
+                                     K=K)
+                params = agent.get_genome()
+                params['K'] = K
+                model = mm.Model(**params)
+                stats = envs.trial_multi_model(models=[model],
+                                               environment=env,
+                                               nb_trials=nb_trials,
+                                               nb_rounds=nb_rounds,
+                                               nb_reps=self.nb_reps)
+
+                fitness += calc_fitness(stats=stats)[0]
+
+        return (fitness / (len(Ks) + 3),)
+
+
+
+def calc_fitness2(stats: dict) -> float:
+
+    """
+    Calculate the fitness value.
+
+    Parameters
+    ----------
+    stats : dict
+        The statistics of the agent.
+
+    Returns
+    -------
+    fitness : float
+        The fitness value.
+    """
+
+    fitness = stats["mean_scores"][0].mean(axis=0).mean(axis=0)
+    assert isinstance(fitness, float), f"fitness is not a float: {fitness}"
+
+    return (fitness,)
+
+
+
