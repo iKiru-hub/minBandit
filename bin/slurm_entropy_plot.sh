6bff1680a536e356ea5d30c3e137776f93d88f03
Krubeal <dkirubel@gmail.com>
2024-12-16 23:34:59 +0100

    fast update

--- bin/slurm_entropy_plot.sh
+++ bin/slurm_entropy_plot.sh
@@ -3,7 +3,7 @@
 #SBATCH -p milanq #ipuq #milanq #armq #milanq #fpgaq #milanq # partition (queue)
 #SBATCH -N 1 # number of nodes
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=128
+#SBATCH --cpus-per-task=32
 ##SBATCH --mem-per-cpu=1GB
 #SBATCH --time=1-10:00
 #SBATCH -o /home/daniekru/slurm.column.%j.%N.out # STDOUT
@@ -15,7 +15,7 @@ echo "ecl1 activated"
 cd ~/lab/minBandit/src
 echo "$(pwd)"
 
-srun python3 entropy_run.py --reps 128 --cores 128
+srun python3 entropy_run.py --reps 32 --cores 32
 
 echo "finished"
 
